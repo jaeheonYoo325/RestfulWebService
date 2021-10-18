5dbe64423bf944472242f50920c0bedf7403b82a
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-18 23:11:11 +0900

    2021.10.18 : 유효성 체크를 위한 Validation API 사용.

--- src/main/java/com/restful/restfulwebservice/user/User.java
+++ src/main/java/com/restful/restfulwebservice/user/User.java
@@ -2,6 +2,9 @@ package com.restful.restfulwebservice.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +13,10 @@ import lombok.Data;
 public class User {
 
 	private Integer id;
+	
+	@Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
 	private String name;
+	
+	@Past
 	private Date joinDate;
 }
