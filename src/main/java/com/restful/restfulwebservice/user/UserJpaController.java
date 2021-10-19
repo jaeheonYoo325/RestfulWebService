a00bf597455957488e4abc0aa2cd60e01118928b
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-19 19:19:20 +0900

    2021.10.19 : JPA를 이용한 사용자 추가와 삭제 - POST_DELETE HTTP Method.

--- src/main/java/com/restful/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/restful/restfulwebservice/user/UserJpaController.java
@@ -3,17 +3,24 @@ package com.restful.restfulwebservice.user;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/jpa")
@@ -41,5 +48,23 @@ public class UserJpaController {
 		entityModel.add(linkTo.withRel("all-users"));
 		
 		return ResponseEntity.ok(entityModel);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		User savedUser = userRepository.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 }
